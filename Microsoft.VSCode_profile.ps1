529ed73c2a2f93d0ffabf6eba4ecda2d5a81e08b
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-09-06 15:56:29 -0400

    VSCode Profile

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -1,4 +1,6 @@
+# Load Functions, Aliases, and Completion
 $psdir = (Split-Path -parent $profile)
 
 . "$psdir\profile_functions.ps1"
 . "$psdir\profile_aliases.ps1"
+. "$psdir\profile_completion.ps1"
