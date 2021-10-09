271644e84ced06ce906735d208b78424f4a439ca
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-10-08 20:10:33 -0400

    feat: extract-icon

--- Scripts/Extract-Icon.ps1
+++ Scripts/Extract-Icon.ps1
@@ -1,24 +1,24 @@
 #requires -version 5.1
 
 # inspired by https://community.spiceworks.com/topic/592770-extract-icon-from-exe-powershell
-Function Extract-Icon {
-    [CmdletBinding(SupportsShouldProcess)]
-    Param(
-        [Parameter(Position = 0, Mandatory, HelpMessage = "Specify the path to the file.")]
-        [ValidateScript({ Test-Path $_ })]
-        [string]$Path,
 
-        [Parameter(HelpMessage = "Specify the folder to save the file.")]
-        [ValidateScript({ Test-Path $_ })]
-        [string]$Destination = ".",
+[CmdletBinding(SupportsShouldProcess)]
+Param(
+    [Parameter(Position = 0, Mandatory,HelpMessage = "Specify the path to the file.")]
+    [ValidateScript({Test-Path $_})]
+    [string]$Path,
 
-        [parameter(HelpMessage = "Specify an alternate base name for the new image file. Otherwise, the source name will be used.")]
-        [ValidateNotNullOrEmpty()]
-        [string]$Name,
+    [Parameter(HelpMessage = "Specify the folder to save the file.")]
+    [ValidateScript({Test-Path $_})]
+    [string]$Destination = ".",
 
-        [Parameter(HelpMessage = "What format do you want to use? The default is png.")]
-        [ValidateSet("ico", "bmp", "png", "jpg", "gif")]
-        [string]$Format = "png"
+    [parameter(HelpMessage = "Specify an alternate base name for the new image file. Otherwise, the source name will be used.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Name,
+
+    [Parameter(HelpMessage = "What format do you want to use? The default is png.")]
+    [ValidateSet("ico","bmp","png","jpg","gif")]
+    [string]$Format = "png"
     )
 
     Write-Verbose "Starting $($MyInvocation.MyCommand)"
@@ -32,11 +32,11 @@ Function Extract-Icon {
     }
 
     Switch ($format) {
-        "ico" { $ImageFormat = "icon" }
-        "bmp" { $ImageFormat = "Bmp" }
-        "png" { $ImageFormat = "Png" }
-        "jpg" { $ImageFormat = "Jpeg" }
-        "gif" { $ImageFormat = "Gif" }
+        "ico" {$ImageFormat = "icon"}
+        "bmp" {$ImageFormat = "Bmp"}
+        "png" {$ImageFormat = "Png"}
+        "jpg" {$ImageFormat = "Jpeg"}
+        "gif" {$ImageFormat = "Gif"}
     }
 
     $file = Get-Item $path
@@ -55,12 +55,12 @@ Function Extract-Icon {
     $out = Join-Path -Path $Destination -ChildPath "$base.$format"
 
     Write-Verbose "Extracting $ImageFormat image to $out"
-    $ico = [System.Drawing.Icon]::ExtractAssociatedIcon($file.FullName)
+    $ico =  [System.Drawing.Icon]::ExtractAssociatedIcon($file.FullName)
 
     if ($ico) {
         #WhatIf (target, action)
         if ($PSCmdlet.ShouldProcess($out, "Extract icon")) {
-            $ico.ToBitmap().Save($Out, $Imageformat)
+            $ico.ToBitmap().Save($Out,$Imageformat)
             Get-Item -path $out
         }
     }
@@ -70,4 +70,3 @@ Function Extract-Icon {
     }
 
     Write-Verbose "Ending $($MyInvocation.MyCommand)"
-}
