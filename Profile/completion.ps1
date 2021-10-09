7ab0697fd9c8f24937b180e153fe42dc73527941
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-10-08 20:19:48 -0400

    bug: fix completion script root

--- Profile/completion.ps1
+++ Profile/completion.ps1
@@ -4,8 +4,8 @@
 
 Import-Module Microsoft.PowerShell.Utility
 
-$currpath = (Get-Location).ProviderPath
-$files = Get-ChildItem -Path "$currpath\completions" -Filter "*.ps1"
+$path = "$PSScriptRoot\completions"
+$files = Get-ChildItem -Path $path -Filter "*.ps1"
 ForEach ($file in $files) {
   . $file
 }
