1e57ae5d54a47aacc200153cfc2c0f209e5531ed
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-11-06 18:02:38 -0400

    feat: tweak aliases

--- Profile/aliases.ps1
+++ Profile/aliases.ps1
@@ -4,24 +4,15 @@
 
 Set-Alias -Name pro -Value Edit-Profile
 Set-Alias -Name aliases -Value Get-Alias
-Set-Alias -Name cpkgs -Value chocopkgs
-Set-Alias -Name cclean -Value chococlean
-Set-Alias -Name csearch -Value chocosearch
-Set-Alias -Name cup -Value chocoupgrade
-Set-Alias -Name cpkgs -Value chocopkgs
-Set-Alias -Name cbackup -Value chocobackup
 Set-Alias -Name refresh -Value refreshenv
 Set-Alias -Name touch -Value New-File
 Set-Alias -Name rproj -Value openrproj
 Set-Alias -Name chkdisk -Value Check-Disk
 Set-Alias -Name cdd -Value CreateAndSet-Directory
 Set-Alias -Name emptytrash -Value Clear-RecycleBin
-Set-Alias -Name codee -Value code-insiders
-Set-Alias -Name gcal -Value gcalcli
-Set-Alias -Name agenda -Value Get-Agenda
-Set-Alias -Name gcalm -Value Get-CalendarMonth
-Set-Alias -Name gcalw -Value Get-CalendarWeek
-Set-Alias -Name gcalnew -Value New-CalendarEvent
+
+# Remove stupid 'touch' alias for 'set-filetime'
+Remove-Alias -Name touch 
 
 # Ensure `R` is for launching an R Terminal:
 if (Get-Command R.exe -ErrorAction SilentlyContinue | Test-Path) {
@@ -32,5 +23,35 @@ if (Get-Command R.exe -ErrorAction SilentlyContinue | Test-Path) {
 # Ensure gpg points to correct program
 # Set-Alias -Name gpg -Value 'C:\Program Files (x86)\GnuPG\bin\gpg.exe'
 
-# Remove stupid 'touch' alias for 'set-filetime'
-Remove-Alias -Name touch 
+# Chocolatey
+If (Get-Command choco -ErrorAction SilentlyContinue) {
+	Set-Alias -Name cpkgs -Value chocopkgs
+	Set-Alias -Name csearch -Value chocosearch
+	Set-Alias -Name cup -Value chocoupgrade
+
+	If (Get-Command choco-cleaner) {
+		Set-Alias -Name cclean -Value chococlean
+	}
+	If (Get-Command choco-package-list-backup) {
+		Set-Alias -Name cbackup -Value chocobackup
+	}
+}
+
+# gcalcli
+If (Get-Command gcalcli -ErrorAction SilentlyContinue) {
+	Set-Alias -Name gcal -Value gcalcli
+	Set-Alias -Name agenda -Value Get-Agenda
+	Set-Alias -Name gcalm -Value Get-CalendarMonth
+	Set-Alias -Name gcalw -Value Get-CalendarWeek
+	Set-Alias -Name gcalnew -Value New-CalendarEvent
+}
+
+# If using code-insiders
+If (Get-Command code-insiders -ErrorAction SilentlyContinue) {
+	Set-Alias -Name codee -Value code-insiders
+}
+
+# lsd
+If (Get-Command lsd -ErrorAction SilentlyContinue) {
+  ${function:lsa} = { & lsd -a }
+}
