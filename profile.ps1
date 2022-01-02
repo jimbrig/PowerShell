8f24b22f47b19050ca09261b4c84e7025636ef18
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-01-02 00:51:35 -0500

    feat: add to prompt pwsh version and exec. policy

--- profile.ps1
+++ profile.ps1
@@ -21,6 +21,9 @@ $PSDefaultParameterValues = @{
 # Prompt
 Set-PoshPrompt -Theme wopian -ErrorAction SilentlyContinue
 
+# Write Current Version and Execution Policy Details:
+Write-Host "PowerShell Version: $($psversiontable.psversion) - ExecutionPolicy: $(Get-ExecutionPolicy)" -ForegroundColor yellow
+
 # ZLocation must be after all prompt changes:
 Import-Module ZLocation
 Write-Host -Foreground Green "`n[ZLocation] knows about $((Get-ZLocation).Keys.Count) locations.`n"
