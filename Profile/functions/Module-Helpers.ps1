2a837244b765ed73a65f81219c90c02ab1923a19
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-01-02 01:50:09 -0500

    feat: create/edit modules options prompt and helpers

---
+++ Profile/functions/Module-Helpers.ps1
@@ -0,0 +1,37 @@
+# --------
+# Helpers
+# --------
+
+Function Backup-Modules {
+  $modpath = ($env:PSModulePath -split ";")[0]
+  $ymlpath = "$modpath\modules.yml"
+  $jsonpath = "$modpath\modules.json"
+  $mods = (Get-ChildItem $modpath -Directory).Name
+  If (!(Get-Module -Name powershell-yaml -ErrorAction SilentlyContinue)) {
+    ConvertTo-Json $mods > $jsonpath
+  } else {
+    ConvertTo-Yaml -Data $mods -OutFile $ymlpath -Force
+  }
+
+}
+
+Function Sync-Modules {
+  $psdir = (Split-Path -Parent $profile)
+  Set-Location $psdir
+  git pull
+  git add PowerShell/Modules/**
+  git commit -m "config: Updated modules configurations"
+  git-cliff -o "$HOME\Documents\CHANGELOG.md"
+  git add CHANGELOG.md
+  git commit -m "doc: update CHANGELOG.md for added modules"
+  git push
+}
+
+Function Restore-Modules {
+  $mods = Get-Content ~/.dotfiles/powershell/modules.json | ConvertFrom-Json
+
+  foreach ($mod in $mods) {
+    Write-Host "Installing PowerShell Module for Current User: $mod" -ForegroundColor Yellow
+    Install-Module $mod -Scope CurrentUser -Force
+  }
+}
--- Profile/modules.ps1
+++ Profile/modules.ps1
@@ -2,49 +2,15 @@
 # Modules and Helper Functions
 # -----------------------------
 
+# Required PSReadLine Beta Version
+If (!(!((Get-Module -Name PSReadLine).Version.Major -ge 2)) -and (!(Get-Module -Name PSReadLine).Version.Minor -ge 2)) {
+    Install-Module -Name PSReadLine -AllowPrerelease -Force -AllowClobber -Scope CurrentUser
+}
+
+Import-Module -Name PSReadLine
 Import-Module -Name posh-git
 Import-Module -Name oh-my-posh
 Import-Module -Name Terminal-Icons
-Import-Module -Name PSReadLine
 
 # Enable Posh-Git
 $env:POSH_GIT_ENABLED = $true
-
-# --------
-# Helpers
-# --------
-
-Function Backup-Modules {
-  $modpath = ($env:PSModulePath -split ";")[0]
-  $ymlpath = "$modpath\modules.yml"
-  $jsonpath = "$modpath\modules.json"
-  $mods = (Get-ChildItem $modpath -Directory).Name
-  If (!(Get-Module -Name powershell-yaml -ErrorAction SilentlyContinue)) {
-    ConvertTo-Json $mods > $jsonpath
-  } else {
-    ConvertTo-Yaml -Data $mods -OutFile $ymlpath -Force  
-  }
-  
-}
-
-Function Sync-Modules {
-  $psdir = (Split-Path -Parent $profile)
-  Set-Location $psdir
-  git pull
-  git add PowerShell/Modules/**
-  git commit -m "config: Updated modules configurations"
-  git-cliff -o "$HOME\Documents\CHANGELOG.md"
-  git add CHANGELOG.md
-  git commit -m "doc: update CHANGELOG.md for added modules"
-  git push
-}
-
-Function Restore-Modules {
-  $mods = Get-Content ~/.dotfiles/powershell/modules.json | ConvertFrom-Json
-
-  foreach ($mod in $mods) {
-    Write-Host "Installing PowerShell Module for Current User: $mod" -ForegroundColor Yellow
-    Install-Module $mod -Scope CurrentUser -Force
-  }
-}
-
