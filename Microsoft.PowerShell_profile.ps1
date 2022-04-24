eaab1e10f5155861b887bb504d5f2fcd968c6641
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-04-23 21:34:24 -0400

    feat: add choco to CurrentUserCurrentHost

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,13 @@
 
 # Load Custom User Specific Functions Here:
 # . Custom\*.ps1
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
