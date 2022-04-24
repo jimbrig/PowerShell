cd9f9af94c26ec6e597c91d0235c9258cf008885
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-04-23 21:34:05 -0400

    bug: quotes

--- Profile/completion.ps1
+++ Profile/completion.ps1
@@ -5,7 +5,7 @@
 Import-Module Microsoft.PowerShell.Utility
 
 $path = "$PSScriptRoot\completions"
-$files = Get-ChildItem -Path $path -Filter "*.ps1"
+$files = Get-ChildItem -Path $path -Filter '*.ps1'
 ForEach ($file in $files) {
   . $file
 }
